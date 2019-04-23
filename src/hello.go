1190c98068a658c6b169ee62fc3d798d35106960
zhouxiang <zhouxiang@peilian.com>
2019-04-23 21:55:17 +0800

    method add

--- src/hello.go
+++ src/hello.go
@@ -1,10 +1,18 @@
-
 package main
 
 import (
 	"fmt"
 )
 
-func main(){
+func add(a int, b int) int {
+	var sum int
+	sum = a + b
+	return sum
+}
+
+func main() {
 	fmt.Println("Hello World!")
+	var sum int
+	sum = add(100, 5)
+	fmt.Println("sum = ", sum)
 }
